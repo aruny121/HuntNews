affe8d02584e627313389654efc075e9518bf257
arunyadav123 <aruny121@gmail.com>
2020-07-27 11:56:58 +0530

    headline main page completed

--- app/src/main/java/com/example/getnews/Adapters/HeadlineNewsAdapter.java
+++ app/src/main/java/com/example/getnews/Adapters/HeadlineNewsAdapter.java
@@ -15,7 +15,13 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.example.getnews.Model.ArticleModel;
 import com.example.getnews.R;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 
 public class HeadlineNewsAdapter extends RecyclerView.Adapter<HeadlineNewsAdapter.ViewHolder> {
@@ -38,9 +44,10 @@ public class HeadlineNewsAdapter extends RecyclerView.Adapter<HeadlineNewsAdapte
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
         holder.mItem = mValues.get(position);
+        holder.language.setText(NullcheckforResponse(mValues.get(position).getTitle()));
+        holder.author.setText(NullcheckforResponse(mValues.get(position).getAuthor()));
 
-        holder.language.setText(mValues.get(position).getTitle());
-
+        holder.date.setText(parsedate(mValues.get(position).getPublishedAt()));
         Glide.with(mContext).load(mValues.get(position).getUrlToImage())
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .into(holder.avatar);
@@ -59,7 +66,7 @@ public class HeadlineNewsAdapter extends RecyclerView.Adapter<HeadlineNewsAdapte
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         public final View mView;
-        public final TextView  language;
+        public final TextView  language, author, date;
         public final TextView mContentView;
         public final ImageView avatar;
 
@@ -71,12 +78,45 @@ public class HeadlineNewsAdapter extends RecyclerView.Adapter<HeadlineNewsAdapte
             mContentView = (TextView) view.findViewById(R.id.content);
             avatar = (ImageView) view.findViewById(R.id.avatar);
             language = (TextView) view.findViewById(R.id.language);
+            author = (TextView) view.findViewById(R.id.author);
+            date = (TextView) view.findViewById(R.id.date);
+
 
         }
 
         @Override
         public String toString() {
             return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+
+
+    public  String parsedate(String date){
+        if(date == null)
+            return "Unknown";
+
+        SimpleDateFormat inputFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        SimpleDateFormat outputFormatter = new SimpleDateFormat("yyyy MM dd");
+        Date dateformatter = null;
+        try {
+            dateformatter = inputFormatter.parse(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+       return outputFormatter.format(dateformatter);
+
+    }
+
+
+    public  String  NullcheckforResponse(String response)
+    {
+        if (response == null)
+        {
+            return "Unknown";
+        }
+        else
+        {
+            return response;
         }
     }
 }
